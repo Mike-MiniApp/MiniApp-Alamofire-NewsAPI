043111c2e0886032bc4a51a2b81ba14a0bf1cb76
mikekondo <foad5969@gmail.com>
2022-04-05 14:58:10 +0900

    動作確認完了

--- MiniApp-Alamofire-NewsAPI/RequestNewsData.swift
+++ MiniApp-Alamofire-NewsAPI/RequestNewsData.swift
@@ -15,14 +15,17 @@ class RequestNewsData{
     var delegete: CatchNewsDataDelegate?
     func request(index: Int,searchWord: String){
         // let urlString = "https://newsapi.org/v2/top-headlines?country=jp&category=\(searchWord)&apiKey=2dadbe9531074cfe86934358180c7e24"
-        let urlString = "https://newsapi.org/v2/top-headlines?country=jp&category=technology&apiKey=2dadbe9531074cfe86934358180c7e24"
+        let urlString = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=2dadbe9531074cfe86934358180c7e24"
+        // let urlString = "https://newsapi.org/v2/everything?q=apple&from=2022-04-04&to=2022-04-04&sortBy=popularity&apiKey=2dadbe9531074cfe86934358180c7e24"
         print("index:",index)
         AF.request(urlString).responseJSON { response in
             guard let data = response.data else{
+                print("nodata")
                 return
             }
             do{
-                let newsData = try JSONDecoder().decode(Articles.self, from: data)
+                let newsData = try JSONDecoder().decode(Articles.self, from: data) // カテゴリーの指定によってエラーが起きてcatchしてしまう。
+                print("nesData",newsData)
                 let title = newsData.articles[index].title
                 let url = newsData.articles[index].url
                 let urlToImage = newsData.articles[index].urlToImage
